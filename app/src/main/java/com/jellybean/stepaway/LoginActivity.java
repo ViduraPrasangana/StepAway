f9c2928dfedf76bb0d933740ab07fbdddfce03bb
Vidura Prasangana <vidura.prasangana16@gmail.com>
2021-12-19 23:35:03 +0530

    advertise data

--- app/src/main/java/com/jellybean/stepaway/LoginActivity.java
+++ app/src/main/java/com/jellybean/stepaway/LoginActivity.java
@@ -3,12 +3,30 @@ package com.jellybean.stepaway;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.chaos.view.PinView;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText phone;
+    Button sendBtn;
+    PinView otpView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        phone = findViewById(R.id.phone);
+        sendBtn = findViewById(R.id.sendBtn);
+        otpView = findViewById(R.id.otp_view);
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                otpView.setEnabled(true);
+            }
+        });
     }
 }
